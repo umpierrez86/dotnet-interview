9458e12756dd91838503006ec1fb977d1aac9117
Martín Fernández <mfernandez@crunchloop.io>
2024-11-26 11:12:48 -0300

    chore: Add csharpier

--- TodoApi/Models/TodoList.cs
+++ TodoApi/Models/TodoList.cs
@@ -2,6 +2,6 @@ namespace TodoApi.Models;
 
 public class TodoList
 {
-  public long Id { get; set; }
-  public required string Name { get; set; }
+    public long Id { get; set; }
+    public required string Name { get; set; }
 }
