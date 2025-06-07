638924a986b95e6b1e2881efa6f7d0724d0d0140
Luciano Umpierrez <umpierrez86@gmail.com>
2025-06-07 14:31:02 -0300

    fix: changed list repo to Infraestructure

---
+++ Todo.Infrastructure/Repositories/TodoListRepository.cs
@@ -0,0 +1,30 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using TodoMcpServer.Interfaces;
+
+namespace Todo.Infrastructure.Repositories;
+
+public class TodoListRepository(DbContext context) : Repository<TodoList>(context), ITodoListRepository
+{
+    private readonly DbSet<TodoList> _entities = context.Set<TodoList>();
+    
+    public async Task<List<TodoList>> GetAllWithItems(Expression<Func<TodoList, bool>>? predicate = null)
+    {
+        return predicate == null ? await _entities.Include(list => list.Items).ToListAsync() 
+            : await _entities.Where(predicate).Include(list => list.Items).ToListAsync();
+    }
+    
+    public async Task<TodoList> GetWithItems(Expression<Func<TodoList, bool>> predicate)
+    {
+        var entity = await _entities.Include(list => list.Items)
+            .FirstOrDefaultAsync(predicate);
+
+        if(entity == null)
+        {
+            throw new ArgumentException($"Todo list not found");
+        }
+
+        return entity;
+    }
+}
